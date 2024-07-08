f9958e9e7caa3d7ca6c20dda518acb7da53551d3
İdris Akan <idrisakan@Idris-MacBook-Pro.local>
2024-07-08 13:14:42 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Portfolyo Sitem
+
+
+### Son dönemde, geliştirdiğim projelerde modern ve güçlü teknolojiler kullanarak harika deneyimler oluşturdum. İşte portfolyomda yer alan bazı teknolojiler:
+
+- @tailwindcss/typography (^0.5.13): Metin içeriklerinin kolay ve şık bir şekilde stilize edilmesini sağlıyor.
+- framer-motion (^11.2.10): Web uygulamalarımda etkileyici ve akıcı animasyonlar oluşturmak için kullanıyorum.
+- graphql (^16.8.2) ve graphql-request (^7.0.1): Verileri daha verimli ve esnek bir şekilde yönetmemi sağlayan bu güçlü ikili, API sorgularımda büyük kolaylık sağlıyor.
+- next (14.2.4): React tabanlı projelerimde sunucu tarafı rendering ve statik site üretimi gibi ileri seviye özellikler sunarak, performansı ve SEO'yu artırıyor.
+- next-themes (^0.3.0): Uygulamalarımda tema desteğini kolayca eklememi sağlıyor, kullanıcı deneyimini kişiselleştirmemi mümkün kılıyor.
+- nextjs-progressbar (^0.0.16): Sayfa yüklemelerinde kullanıcı deneyimini iyileştiren şık bir yükleme çubuğu eklememi sağlıyor.
+- react (^18) ve react-dom (^18): Kullanıcı arayüzlerimi oluşturmam ve yönetmem için vazgeçilmez kütüphaneler.
+- react-typical (^0.1.3): Dinamik yazı efektleri ekleyerek projelerimi daha çekici hale getiriyor.
+
+### Bu teknolojilerle geliştirdiğim projeler, kullanıcı odaklı, performanslı ve estetik açıdan tatmin edici çözümler sunmama olanak tanıyor. Portfolyomla ilgileniyorsanız, daha fazla detay için profilime göz atabilir veya bana ulaşabilirsiniz!
+
+### Her türlü geri bildiriminizi ve bağlantınızı bekliyorum. Hepinize başarılı projeler dilerim! 🚀💻
+
+
+<img src="portfolyo.gif" /># Portfolyom
---
+++ pages/index.js
@@ -0,0 +1,152 @@
+import Link from "next/link";
+import React from "react";
+import Layout from "@/components/Layout";
+import { motion } from "framer-motion";
+import Typical from "react-typical";
+
+import GithubIcon from "../components/icons/Github.js";
+import TwitterIcon from "../components/icons/Twitter.js";
+import LinkedinIcon from "../components/icons/Linkedin.js";
+
+
+const TypingAnimation = React.memo(
+  () => {
+    return (
+      <Typical
+        loop={3}
+        wrapper="p"
+        steps={[
+          "React Developer",
+          2000,
+          "React Native Developer",
+          2000,
+          "React Enthusiast",
+          2000,
+        ]}
+      />
+    );
+  },
+  (props, prevProps) => true
+);
+
+export default function Home() {
+  return (
+    <Layout title={"Portfolyo"} description={"My Portfolyo"}>
+      <motion.img
+        src="/images/ball-1.svg"
+        alt="ball-1"
+        className="hidden md:block h-auto absolute top-48 right-10 z-[1]"
+        whileHover={{ y: -10, rotate: 90, transition: { duration: 0.4 } }}
+        dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+      />
+      <motion.img
+        src="/images/ball-2.svg"
+        alt="ball-2"
+        className="hidden md:block h-auto absolute bottom-10 left-10 z-[1]"
+        whileHover={{ y: -10, rotate: 90, transition: { duration: 0.4 } }}
+        dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+      />
+      <main className="w-full h-full">
+        <section className="h-full max-w-5xl mx-auto pt-16 md:pt-8">
+          <div className="flex flex-col items-center justify-center md:items-start">
+            <h4 className="text-lg text-gray-800 dark:text-white">
+              <span className="text-xl font-normal">Hello,</span> I'm
+            </h4>
+            <h1 className="mt-5 text-xl font-extrabold tracking-normal text-gray-800 uppercase md:text-4xl md:mt-3 dark:text-white">
+              Idris AKAN
+            </h1>
+            <div className="mt-5 text-2xl font-normal text-transparent bg-gradient-to-r from-pink-500 via-indigo-500 to-purple-700 bg-clip-text md:mt-3">
+              <TypingAnimation />
+            </div>
+            <div className="flex items-center mt-8 space-x-6 md:mt-4">
+              <motion.a
+                href="https://github.com/idrisakan"
+                target={"_blank"}
+                rel="noopener noreferrer"
+                className="flex items-center justify-center text-gray-800 transition-colors duration-300 dark:text-gray-100 dark:hover:text-pinkish-200 hover:text-pinkish-200"
+                whileHover={{ scale: 1.1 }}
+              >
+                <GithubIcon className={"w-8 h-8 fill-current"} />
+              </motion.a>
+              <motion.a
+                href="https://x.com/?lang=tr"
+                target={"_blank"}
+                rel="noopener noreferrer"
+                className="flex items-center justify-center text-gray-800 transition-colors duration-300 dark:text-gray-100 dark:hover:text-pinkish-200 hover:text-pinkish-200"
+                whileHover={{ scale: 1.1 }}
+              >
+                <TwitterIcon className={"w-8 h-8 fill-current"} />
+              </motion.a>
+              <motion.a
+                href="https://www.linkedin.com/in/idris-akan-0bb4b32a9/"
+                target={"_blank"}
+                rel="noopener noreferrer"
+                className="flex items-center justify-center text-gray-800 transition-colors duration-300 dark:text-gray-100 dark:hover:text-pinkish-200 hover:text-pinkish-200"
+                whileHover={{ scale: 1.1 }}
+              >
+                <LinkedinIcon className={"w-8 h-8 fill-current"} />
+              </motion.a>
+            </div>
+          </div>
+          {/* Code Area */}
+          <article className="mt-3 prose md:mt-4 md:prose-lg dark:prose-dark ">
+            <div>
+              <pre>
+                <code className="language-js">
+                  <div className="flex items-center py-0 my-0 space-x-4 text-gray-500">
+                    <span>// Portfolio.js </span>
+                  </div>
+                  <p>
+                    <span className="text-gray-500">//</span>{" "}
+                    <span className="text-purple-300">Todo:</span> You can drag
+                    these balloons I'm really in love with them 🎈🎈
+                  </p>
+
+                  <div className="flex items-center space-x-4">
+                    <img
+                      src="https://avatars.githubusercontent.com/u/155467293?s=400&u=5c5c9bb43c7c52007da13e25bf51d0a5c0eda664&v=4"
+                      alt="Me"
+                      className="w-10 h-10 rounded-full"
+                    />
+
+                    <div className="">
+                      <p className="text-sm">
+                        <span className="text-pink-300"> {">"} </span> const
+                        techWeb
+                        <span className="font-bold text-pink-300">2</span> =
+                        ["Reactjs", "Nextjs", "Graphql", "Nodejs"];{" "}
+                      </p>
+                      <p className="text-sm">
+                        <span className="text-pink-300"> {">"} </span> const
+                        techWeb
+                        <span className="font-bold text-pink-300">3</span> =
+                        ["Solidity", "Web3.js", "Ethers.js"];{" "}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="flex items-center space-x-8">
+                    <Link className="transition duration-300" href="/projects">
+                      Projects
+                    </Link>
+
+                    <a
+                      href="https://www.youtube.com/"
+                      target={"_blank"}
+                      rel="noopener noreferrer"
+                      className="transition duration-300"
+                    >
+                      Youtube
+                    </a>
+                  </div>
+                </code>
+              </pre>
+            </div>
+          </article>
+        </section>
+      </main>
+    </Layout>
+  );
+}
+
+
